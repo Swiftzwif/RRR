2fc54b4625f953caec50c332821b52171929e1ab
Jaymison Sanchez <jaymisonsanchez25@gmail.com>
2025-10-30 12:39:44 -0400

    fix: change font display to prevent flash during load

--- apps/trajectory2/src/app/layout.tsx
+++ apps/trajectory2/src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({
   variable: "--font-body",
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-  display: "swap",
+  display: "block",
 });
 
 export const metadata: Metadata = {
